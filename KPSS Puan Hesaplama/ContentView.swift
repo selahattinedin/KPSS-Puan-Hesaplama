7925b0190bad7a56f6256f2700423aefe70f2fc9
Selahattin Edin <selahattinedin1@gmail.com>
2024-08-08 17:23:13 +0300

    Initial Commit

---
+++ KPSS Puan Hesaplama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KPSS Puan Hesaplama
+//
+//  Created by Selahattin EDİN on 8.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KPSS Puan Hesaplama/KPSS_Puan_HesaplamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPSS_Puan_HesaplamaApp.swift
+//  KPSS Puan Hesaplama
+//
+//  Created by Selahattin EDİN on 8.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct KPSS_Puan_HesaplamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
