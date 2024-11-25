0fccfa6a6bb5eb592bf91f657fcbef4011f301ec
Selahattin Edin <selahattinedin1@gmail.com>
2024-11-25 21:23:54 +0300

    OabtView and OabtViewViewModel updated

--- KPSS Puan Hesaplama/ViewModels/OabtViewViewModel.swift
+++ KPSS Puan Hesaplama/ViewModels/OabtViewViewModel.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+class EgitimBilimleriViewModel: ObservableObject {
+    @Published var gkDogruSayisi: Double = 40
+    @Published var gkYanlisSayisi: Double = 10
+    @Published var gyDogruSayisi: Double = 30
+    @Published var gyYanlisSayisi: Double = 0
+    @Published var ebDogruSayisi: Double = 40
+    @Published var ebYanlisSayisi: Double = 0
+    
+    @Published var sonuc2022: Double = 0
+    @Published var sonuc2023: Double = 0
+    @Published var EBsonuc2022: Double = 0
+    @Published var EBsonuc2023: Double = 0
+    @Published var isShowingSheet: Bool = false
+    
+    func hesaplaSonuc() {
+        let gkNet = gkDogruSayisi - (gkYanlisSayisi / 4)
+        let gyNet = gyDogruSayisi - (gyYanlisSayisi / 4)
+        let ebNet = ebDogruSayisi - (ebYanlisSayisi / 4)
+        
+        EBsonuc2022 = Constants.eb2022Puan +
+            gyNet * Constants.eb2022GYKatsayi +
+            gkNet * Constants.eb2022GKKatsayi +
+            ebNet * Constants.eb2022Katsayi
+        
+        sonuc2022 = Constants.lisans2022Puan +
+            gyNet * Constants.lisans2022GYKatsayi +
+            gkNet * Constants.lisans2022GKKatsayi
+        
+        EBsonuc2023 = Constants.eb2023Puan +
+            gyNet * Constants.eb2023GYKatsayi +
+            gkNet * Constants.eb2023GKKatsayi +
+            ebNet * Constants.eb2023Katsayi
+        
+        sonuc2023 = Constants.lisans2023Puan +
+            gyNet * Constants.lisans2023GYKatsayi +
+            gkNet * Constants.lisans2023GKKatsayi
+        
+        isShowingSheet = true
+    }
+
+    var formKontrol: Bool {
+        (gkDogruSayisi + gkYanlisSayisi) > 60 ||
+        (gyDogruSayisi + gyYanlisSayisi) > 60 ||
+        (ebDogruSayisi + ebYanlisSayisi) > 80
+    }
+    
+    var gkHataMesaji: Bool {
+        gkDogruSayisi + gkYanlisSayisi > 60
+    }
+    
+    var gyHataMesaji: Bool {
+        gyDogruSayisi + gyYanlisSayisi > 60
+    }
+    
+    var ebHataMesaji: Bool {
+        ebDogruSayisi + ebYanlisSayisi > 80
+    }
+}
--- KPSS Puan Hesaplama/Views/OABTView.swift
+++ KPSS Puan Hesaplama/Views/OABTView.swift
@@ -9,179 +9,115 @@ import SwiftUI
 
 struct OABTView: View {
 
-    @State var gkDogruSayisi:Double = 40
-    @State var gkYanlisSayisi:Double = 10
-    @State var gyDogruSayisi:Double = 30
-    @State var gyYanlisSayisi:Double = 0
-    @State var ebDogruSayisi:Double = 40
-    @State var ebYanlisSayisi:Double = 0
-    @State var oabtDogruSayisi:Double = 40
-    @State var oabtYanlisSayisi:Double = 0
+        @StateObject private var viewModel = EgitimBilimleriViewModel()
         
-    @State var sonuc2022:Double = 0
-    @State var sonuc2023: Double = 0
-    @State var EBsonuc2022:Double = 0
-    @State var EBsonuc2023:Double = 0
-    @State var oabtsonuc2023:Double = 0
-    @State var oabtsonuc2022:Double = 0
-    
-    @State private var oabtKatsayi = 0.4334
-    @State private var oabtPuan = 43.805
-    
-    @State private var selectedOption = 0
-        let options = [
-            (0.4334,43.805, "Beden Eğitimi"),
-            (0.3666,41.071, "Biyoloji"),
-            (0.4301,39.060, "Coğrafya"),
-            (0.4052,34.908, "Din Kültürü"),
-            (0.3679,42.156, "Edebiyat"),
-            (0.5388,39.313, "Fen Bilgisi"),
-            (0.3817,41.604, "Fizik"),
-            (0.5225,36.309, "İlköğretim Matematik"),
-            (0.3883,37.962, "İmam Hatip Meslek Dersleri Ö."),
-            (0.3791,40.920, "İngilizce"),
-            (0.4542,42.157, "Kimya"),
-            (0.4247,41.759, "Lise Matematik"),
-            (0.4944,33.292, "Okul Öncesi"),
-            (0.4883,29.014, "Rehberlik "),
-            (0.6184,33.598, "Sınıf Öğretmenliği"),
-            (0.6142,34.101, "Sosyal Bilgiler"),
-            (0.3521,41.418, "Tarih"),
-            (0.4565,34.329, "Türkçe")
-        ]
-    
-    @State private var isShowingSheet = false
-
-    
-    var body: some View {
-        NavigationStack {
-            VStack {
-                    
-                Form{
-                        
-                    Section{
-                        Stepper("Doğru Sayisi: \(gkDogruSayisi, specifier: "%.0f")", value: $gkDogruSayisi, in: 0...(60-gkYanlisSayisi))
-                            .sensoryFeedback(.selection, trigger: gkDogruSayisi)
-                            .bold()
-                            
-                        Stepper("Yanlış Sayısı: \(gkYanlisSayisi, specifier: "%.0f")", value: $gkYanlisSayisi, in: 0...(60-gkDogruSayisi))
-                            .sensoryFeedback(.selection, trigger: gkYanlisSayisi)
-                            .bold()
-
-                            
-                        }header: {
-                            Text("Genel Kültür")
-                                .textCase(.none)
-                        }footer: {
-                            if(gkDogruSayisi + gkYanlisSayisi > 60){
-                                Text("Toplam doğru ve yanlış sayıları 60'ı geçemez.")
-                                    .foregroundStyle(.red)
-                            }
-                        }
-                        
-                        
-                    Section{
-                        Stepper("Doğru Sayisi: \(gyDogruSayisi, specifier: "%.0f")", value: $gyDogruSayisi, in: 0...(60 - gyYanlisSayisi))
-                            .sensoryFeedback(.selection, trigger: gyDogruSayisi)
-                            .bold()
-                            
-                        Stepper("Yanlış Sayısı: \(gyYanlisSayisi, specifier: "%.0f")", value: $gyYanlisSayisi, in: 0...(60-gyDogruSayisi))
-                            .sensoryFeedback(.selection, trigger: gyYanlisSayisi)
-                            .bold()
-
-                            
-                        }header: {
-                            Text("Genel Yetenek")
-                                .textCase(.none)
-                        }footer: {
-                            if(gyDogruSayisi + gyYanlisSayisi > 60){
-                                Text("Toplam doğru ve yanlış sayıları 60'ı geçemez.")
-                                    .foregroundStyle(.red)
-                            }
-                        }
-                        
-                        
-                    Section{
-                        Stepper("Doğru Sayisi: \(ebDogruSayisi, specifier: "%.0f")", value: $ebDogruSayisi, in: 0...(80 - ebYanlisSayisi))
-                            .sensoryFeedback(.selection, trigger: ebDogruSayisi)
-                            .bold()
-                            
-                        Stepper("Yanlış Sayısı: \(ebYanlisSayisi, specifier: "%.0f")", value: $ebYanlisSayisi, in: 0...(80-ebDogruSayisi))
-                                .sensoryFeedback(.selection, trigger: ebYanlisSayisi)
-                                .bold()
-
-                            
-                        }header: {
-                            Text("Eğitim Bilimleri")
-                                .textCase(.none)
-                        }footer: {
-                            if(ebDogruSayisi + ebYanlisSayisi > 80){
-                                Text("Toplam doğru ve yanlış sayıları 80'ı geçemez.")
-                                    .foregroundStyle(.red)
-                            }
-                        }
-                    
-                    Section{
-                        Picker("Bölüm Seçiniz", selection: $selectedOption) {
-                            ForEach(0..<options.count, id: \.self){ index in
-                                HStack{
-                                    Image(systemName: "person.circle.fill")
-                                    Text(options[index].2)
-                                }
-                                
-                            }
-                        }
-                        Stepper("Doğru Sayisi: \(oabtDogruSayisi, specifier: "%.0f")", value: $oabtDogruSayisi, in: 0...(75 - oabtYanlisSayisi))
-                            .sensoryFeedback(.selection, trigger: oabtDogruSayisi)
-                            .bold()
-                            
-                        Stepper("Yanlış Sayısı: \(oabtYanlisSayisi, specifier: "%.0f")", value: $oabtYanlisSayisi, in: 0...(75 - oabtDogruSayisi))
-                                .sensoryFeedback(.selection, trigger: oabtYanlisSayisi)
-                                .bold()
-                        
-                        HesaplaButton(title: "Hesapla") {
-                            let gkNet = gkDogruSayisi - (gkYanlisSayisi / 4)
-                            let gyNet = gyDogruSayisi - (gyYanlisSayisi / 4)
-                            let ebNet = ebDogruSayisi - (ebYanlisSayisi / 4)
-                            let oabtNet = oabtDogruSayisi - (oabtYanlisSayisi / 4)
-                                
-                                withAnimation {
-                                    EBsonuc2022 = 36.812 + gyNet * 0.3985 + gkNet * 0.3512 + ebNet * 0.34714
-                                    sonuc2022 = ( 48.616 + gyNet * 0.4756 + gkNet * 0.4192)
-                                    EBsonuc2023 = 40.405 + gyNet * 0.3493 + gkNet * 0.3672 + ebNet * 0.37145
-                                    sonuc2023 =  (51.209 + gyNet * 0.537 + gkNet * 0.418)
-                                    oabtsonuc2022 = oabtPuan + gyNet * 0.1720 + gkNet * 0.1515 + ebNet * 0.1498 + oabtNet * oabtKatsayi
-                                    isShowingSheet.toggle()
-                                }
-                            }
-                            .disabled(formKontrol)
-                            .sensoryFeedback(.selection, trigger: sonuc2022)
-                            .sheet(isPresented: $isShowingSheet) {
-                                SonucView(sonuc2023: sonuc2023, EBsonuc2023: EBsonuc2023, oabtsonuc2023: oabtsonuc2023, oabtsonuc2022: oabtsonuc2022, EBsonuc2022: EBsonuc2022, sonuc2022: sonuc2022)
-                            }
-
-                            
-                        }header: {
-                            Text("ÖABT")
-                                .textCase(.none)
-                        }footer: {
-                            if(oabtDogruSayisi + oabtYanlisSayisi > 75){
-                                Text("Toplam doğru ve yanlış sayıları 75'i geçemez.")
-                                    .foregroundStyle(.red)
-                            }
-                        }
-                    }
-                }.navigationTitle("ÖABT")
+        var body: some View {
+            NavigationStack {
+                Form {
+                    genelKulturSection
+                    genelYetenekSection
+                    egitimBilimleriSection
+                }
+                .navigationTitle("Eğitim Bilimleri")
             }
         }
         
-        var formKontrol:Bool{
-            if((gkDogruSayisi + gkYanlisSayisi) > 60 || (gyDogruSayisi + gyDogruSayisi) > 60 || (ebDogruSayisi + ebYanlisSayisi) > 80 || (oabtDogruSayisi + oabtYanlisSayisi) > 75){
-                true
+        private var genelKulturSection: some View {
+            Section {
+                Stepper("Doğru Sayisi: \(viewModel.gkDogruSayisi, specifier: "%.0f")",
+                        value: $viewModel.gkDogruSayisi,
+                        in: 0...(60-viewModel.gkYanlisSayisi))
+                    .bold()
+                    .sensoryFeedback(.selection, trigger: viewModel.gkDogruSayisi)
+                
+                Stepper("Yanlış Sayısı: \(viewModel.gkYanlisSayisi, specifier: "%.0f")",
+                        value: $viewModel.gkYanlisSayisi,
+                        in: 0...(60-viewModel.gkDogruSayisi))
+                    .bold()
+                    .sensoryFeedback(.selection, trigger: viewModel.gkYanlisSayisi)
+            } header: {
+                Text("Genel Kültür").textCase(.none)
+            } footer: {
+                if viewModel.gkHataMesaji {
+                    Text("Toplam doğru ve yanlış sayıları 60'ı geçemez.")
+                        .foregroundStyle(.red)
+                }
             }
-            return false
+        }
+        
+        private var genelYetenekSection: some View {
+            Section {
+                Stepper("Doğru Sayisi: \(viewModel.gyDogruSayisi, specifier: "%.0f")",
+                        value: $viewModel.gyDogruSayisi,
+                        in: 0...(60-viewModel.gyYanlisSayisi))
+                    .bold()
+                    .sensoryFeedback(.selection, trigger: viewModel.gyDogruSayisi)
+                
+                Stepper("Yanlış Sayısı: \(viewModel.gyYanlisSayisi, specifier: "%.0f")",
+                        value: $viewModel.gyYanlisSayisi,
+                        in: 0...(60-viewModel.gyDogruSayisi))
+                    .bold()
+                    .sensoryFeedback(.selection, trigger: viewModel.gyYanlisSayisi)
+            } header: {
+                Text("Genel Yetenek").textCase(.none)
+            } footer: {
+                if viewModel.gyHataMesaji {
+                    Text("Toplam doğru ve yanlış sayıları 60'ı geçemez.")
+                        .foregroundStyle(.red)
+                }
+            }
+        }
+        
+        private var egitimBilimleriSection: some View {
+            Section {
+                StepperView(title: "Doğru Sayisi",
+                           value: $viewModel.ebDogruSayisi,
+                           maxValue: 80-viewModel.ebYanlisSayisi)
+                
+                StepperView(title: "Yanlış Sayısı",
+                           value: $viewModel.ebYanlisSayisi,
+                           maxValue: 80-viewModel.ebDogruSayisi)
+                
+                HesaplaButton(title: "Hesapla") {
+                    withAnimation {
+                        viewModel.hesaplaSonuc()
+                    }
+                }
+                .disabled(viewModel.formKontrol)
+                .sensoryFeedback(.selection, trigger: viewModel.sonuc2022)
+                .sheet(isPresented: $viewModel.isShowingSheet) {
+                    SonucView(sonuc2023: viewModel.sonuc2023,
+                             EBsonuc2023: viewModel.EBsonuc2023,
+                             oabtsonuc2023: nil,
+                             oabtsonuc2022: nil,
+                             EBsonuc2022: viewModel.EBsonuc2022,
+                             sonuc2022: viewModel.EBsonuc2022)
+                }
+            } header: {
+                Text("Eğitim Bilimleri").textCase(.none)
+            } footer: {
+                if viewModel.ebHataMesaji {
+                    Text("Toplam doğru ve yanlış sayıları 80'ı geçemez.")
+                        .foregroundStyle(.red)
+                }
+            }
         }
     }
+
+    struct StepperView: View {
+        let title: String
+        @Binding var value: Double
+        let maxValue: Double
+        
+        var body: some View {
+            Stepper("\(title): \(value, specifier: "%.0f")",
+                    value: $value,
+                    in: 0...maxValue)
+                .bold()
+                .sensoryFeedback(.selection, trigger: value)
+        }
+    }
+    
 #Preview {
     OABTView()
 }
